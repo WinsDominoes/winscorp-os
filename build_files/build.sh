6fe84497b10f91bb4f8646782d8bfa7131f27c6a
Atius Amy <torv.device027@passmail.net>
2025-05-19 06:54:29 +0700

    add stuff

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,11 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y tmux
+dnf install -y trivalent @"KDE Plasma Workspaces" distrobox
+
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/secureblue.repo
+
+systemctl enable sddm.service
 
 # Use a COPR Example:
 #
