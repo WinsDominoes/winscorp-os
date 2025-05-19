d50d736ce456ccd24088e7e52616d27f7e4b9c9f
Atius Amy <torv.device027@passmail.net>
2025-05-19 07:36:49 +0700

    more packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,15 +11,25 @@ set -ouex pipefail
 dnf install -y 'dnf-command(config-manager)' epel-release
 dnf config-manager --add-repo https://repo.secureblue.dev/secureblue.repo
 dnf config-manager --set-enabled crb
+dnf -y copr enable ublue-os/packages
+dnf config-manager --add-repo https://pkgs.tailscale.com/stable/centos/10/tailscale.repo
 
 # this installs a package from fedora repos
 dnf update -y
-dnf install -y trivalent distrobox plasma-desktop sddm dolphin
+dnf install -y trivalent distrobox plasma-desktop sddm dolphin flatpak ublue-os-just ublue-os-luks ublue-os-signing ublue-os-udev-rules ublue-os-update-services ublue-brew ublue-setup-services ublue-polkit-rules uupd tailscale
 dnf remove -y xwaylandvideobridge
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/secureblue.repo
 
 systemctl enable sddm.service
+systemctl enable brew-setup.service
+systemctl enable tailscaled.service
+systemctl enable fwupd.service
+systemctl enable ublue-system-setup.service
+systemctl --global enable ublue-user-setup.service
+systemctl enable uupd.timer
+
+
 
 # Use a COPR Example:
 #
@@ -31,5 +41,5 @@ systemctl enable sddm.service
 
 #### Example for enabling a System Unit File
 
-dnf config-manager --add-repo repository
+
 
